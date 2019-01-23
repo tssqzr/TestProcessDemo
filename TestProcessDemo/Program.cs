2a3d95ece5e338fbc5e02867bd1f25963b6cfd37
tssqzr <1214661466@qq.com>
2019-01-23 15:21:03 +0800

    初始化工程

---
+++ TestProcessDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace TestProcessDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            queryProcess();
+            Console.WriteLine("Hello World!");
+        }
+
+        static void startProcess()
+        {
+            Process process = Process.Start(
+                new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{""}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+                );
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            Console.WriteLine();
+        }
+
+        static void queryProcess()
+        {
+            var process = Process.GetProcesses();
+            process.ToList().ForEach(p => {
+                Console.WriteLine("pNm:" + p.ProcessName);
+            });
+            Console.WriteLine(process.Count());
+        }
+    }
+}
